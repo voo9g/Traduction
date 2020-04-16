ebd125e562a3c0d50c5d65f7d5be36037a34dada
voo9g <62406604+voo9g@users.noreply.github.com>
2020-04-16 14:37:48 +0400

    Update auth.php

--- Panel/auth.php
+++ Panel/auth.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'not_authorized' => 'Vous n\'êtes pas autoriser à réaliser cette action !',
+    'not_authorized' => 'Vous n\'êtes pas autorisé à réaliser cette action !',
     'auth_error' => 'Une erreur s\'est produite lors de la connexion.',
     'authentication_required' => 'Veuillez vous connecter avant de réaliser cette action !',
-    'remember_me' => 'Se souvenir de moi.',
+    'remember_me' => 'Se souvenir de moi',
     'sign_in' => 'Se connecter',
     'forgot_password' => 'Mot de passe perdu ?',
     'request_reset_text' => 'Avez-vous perdu votre mot de passe ?',
@@ -14,7 +14,7 @@ return [
     'failed' => 'Vos identifiants sont incorrects !',
     'throttle' => 'Trop de tentatives de connexion ! Veuillez recommencer dans :seconds secondes.',
     'password_requirements' => 'Votre mot de passe doit contenir au moins une majuscule, une minuscule, un nombre et doit contenir au minimum 8 caractères.',
-    'request_reset' => 'Locate Account',
+    'request_reset' => 'Localisez voter compte',
     '2fa_required' => 'Authentification à 2 facteurs',
     '2fa_failed' => 'Le code d\'authentification à 2 facteurs est invalide.',
     'totp_failed' => 'Une erreur est survenue lors de la validation du mot de passe à usage unique.',
